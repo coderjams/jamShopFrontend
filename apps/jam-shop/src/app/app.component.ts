3d81049386a73da58dc3f8c78e6cd524858761a4
coderjams <coderjams@gmail.com>
2024-10-27 09:11:49 -0400

    first commit

--- apps/jam-shop/src/app/app.component.ts
+++ apps/jam-shop/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BannerComponent } from '@angular-monorepo/ui'
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, HomePageComponent, ProductListComponent, FooterComponent, HeaderComponent, BannerComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ libs/ui/src/lib/banner/banner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-banner',
+  standalone: true,
+  imports: [],
+  templateUrl: './banner.component.html',
+  styleUrl: './banner.component.sass'
+})
+export class BannerComponent {
+
+}
