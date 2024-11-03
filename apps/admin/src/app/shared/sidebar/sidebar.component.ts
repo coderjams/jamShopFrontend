53970b8197a75e8a12e715880745c6839f6e050e
coderjams <coderjams@gmail.com>
2024-11-03 09:32:07 -0500

    setup dashboard

---
+++ apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-sidebar',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidebar.component.html'
+})
+export class SidebarComponent {
+
+}
